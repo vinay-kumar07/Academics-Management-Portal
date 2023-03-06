31be7f9e922d5bab67aa818651723f0cc919dd19
Vinay Kumar <2020csb1141@iitrpr.ac.in>
2023-03-06 11:18:43 +0530

    test

---
+++ src/test/java/users/AdminTest.java
@@ -0,0 +1,20 @@
+package users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void editCourseCatalog() {
+    }
+
+    @Test
+    void changeAcademicYearSem() {
+    }
+}
